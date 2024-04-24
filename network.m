918829e3491a4d7f6d2b216f1696453dd6fbb5f4
Aleksandra Zinoveva <alisenz@icloud.com>
2024-04-24 23:44:41 +0200

    Implemented a simplistic version of VRE output

--- network.m
+++ network.m
@@ -1,31 +1,48 @@
 %% Problem Formulation
+% Two last generating units are wind and solar based, so no fuel cost
+% there.
 
-%Objective-related:
-a = [16.19, 17.26, 16.6, 16.5, 19.7, 22.26, 27.74, 25.92, 27.27, 27.79];
+%% Objective-related:
 
+% Linear cost coefficient
+a = [16.19, 17.26, 16.6, 16.5, 19.7, 22.26, 27.74, 25.92, 27.27, 27.79, 0, 0];
+
+% Quadratic cost coefficient
 b = [0.00048, 0.00031,  0.002,   0.00211,  0.00398, ... 
-     0.00712, 0.000793, 0.00413, 0.002221, 0.00173];
+     0.00712, 0.000793, 0.00413, 0.002221, 0.00173, 0, 0];
 
-C_run = [1000, 970, 700, 680, 450, 370, 480, 660, 665, 670];
+% Idling cost -- also a constant coefficient in the cost function
+C_run = [1000, 970, 700, 680, 450, 370, 480, 660, 665, 670, 0, 20];
 
-C_start = [9000, 10000, 1100, 1120, 1800, 340, 520, 60, 60, 60];
+% Cost of starting the unit. Stopping is "free".
+C_start = [9000, 10000, 1100, 1120, 1800, 340, 520, 60, 60, 60, 0, 0];
 
-%Constraint-related
+%% Constraint-related
+
+% Demand for 24H
 D = [700,  750,  850,  950,  1000, 1100, ...
      1150, 1200, 1300, 1400, 1450, 1500, ...
      1400, 1300, 1200, 1150, 1000, 1100, ...
      1200, 1400, 1300, 1100, 900,  800];
 
+% Power limit of the conventional units.
 G_max = [455, 455, 130, 130, 162, 80, 85, 55, 55, 55];
 
-T_min = [8, 8, 5, 5, 6, 3, 3, 1, 1, 1];
+% Minimal running time of the units
+T_min = [8, 8, 5, 5, 6, 3, 3, 1, 1, 1, 1, 1];
 
-start = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0]; % starting state of the generators: two first are running
+% Starting state of the generators: two first conventional and wind farm
+% are running
 
-N = 10; T = 24; % number of generators and time periods (24H)
+start = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]; 
+
+Nconv = 10; Nvre = 2; T = 24; % number of generators and time periods (24H)
+N = Nconv + Nvre;
 
 %% Construct Q and c in z'Qz + c'z for price modeling
 
+% VRE units have no influence on construction of Q and c. The idling cost
+% is fixed and other costs do not apply (?)
 Q = [];
 
 for i = 1:N
@@ -44,12 +61,25 @@ c = [repelem(a, T), repelem(C_run, T), repelem(C_start, T)]';
 %     y = [y11, ..., y1T, ..., yNT]
 % and z = [g, x, y]'
 
+
 A1 = eye(N*T);
 
+% Have to sample for VRE on an hourly basis
 A2 = [];
-for i = 1:N
-    A2 = blkdiag(A2, -G_max(i)*eye(T));
+for i = 1:Nconv
+    A2 = blkdiag(A2, blkdiag(G_max(i)*eye(T)));
 end
+
+% Note: figre out how to apply function to an array
+wind_maxpower = [];
+solar_maxpower = [];
+for t = 1:T
+    wind_maxpower = [wind_maxpower, pdf(t, "wind")];
+    solar_maxpower = [solar_maxpower, pdf(t, "solar")];
+end
+
+A2 = -blkdiag(A2, diag(wind_maxpower), diag(solar_maxpower));
+
 
 A3 = [];
 for i = 1:T
@@ -82,7 +112,7 @@ end
 % ------------------------------------------------------
 
 
-A7 = zeros(10, 240);
+A7 = zeros(N, N*T);
 for i = 1:N
     A7(i, 1 + T*(i-1)) = 1;
 end
@@ -112,7 +142,7 @@ v = [zeros(1, N*T), ones(1, 2*N*T), zeros(1, 5*N*T), -D, start]';
 % space -- therefore sparse representation.
 A = sparse(A);
 
-clear A1 A2 A3 A5 A6 Agen t;
+clear A1 A2 A3 A5 A6 A7 Agen t;
 
 % b in constraints Ax=b has become v to not overwrite the network parameters 
 result = solve_gurobi(N,T,Q,c,A,v);
---
+++ pdf.m
@@ -0,0 +1,43 @@
+function [max_power] = pdf(hour, type)
+%PDF Summary of this function goes here
+%   Detailed explanation goes here
+
+%% Forecasted output and probability distributions for ...
+
+% a wind farm
+wind_forecast = 25.5;
+
+wind_probs = [0.5, 0.15, 0.15, 0.10,  0.10;
+              1,   0.99, 1.01, 0.975, 1.025;
+              1,   0.98, 1.02, 0.95,  1.05  ];
+% a solar farm
+solar_forecast = 40;
+
+solar_probs = [0.7, 0.15,  0.15;
+               1,   0.985, 1.015;
+               1,   0.975, 1.025 ];
+%% Start searching through conditions
+
+if type == "wind"
+    if hour <= 12
+        sequence = wind_probs(2, :);
+    else
+        sequence = wind_probs(3, :);
+    end
+    sample = datasample(sequence, 1, 'Weights', wind_probs(1, :));
+    max_power = wind_forecast * sample;
+
+elseif type == "solar"
+    if hour < 7 || hour > 16
+        max_power = 0;
+    else
+        if hour <= 12
+            sequence = solar_probs(2, :);
+        else
+            sequence = solar_probs(3, :);
+        end
+        sample = datasample(sequence, 1, 'Weights', solar_probs(1, :));
+        max_power = solar_forecast * sample;
+    end
+end
+
